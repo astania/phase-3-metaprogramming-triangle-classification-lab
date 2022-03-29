80aa966fa1dd176d6099896896aebd89f29716f7
+ <+>
2022-03-29 14:47:49 -0400

    commit tree 252b2776efad2c214378969a50c7dbbec2e30f80

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
   # write code here
+  attr_accessor :sides
+
+  def initialize(side_1_length, side_2_length, side_3_length)
+    @side_1_length = side_1_length
+    @side_2_length = side_2_length
+    @side_3_length = side_3_length
+
+    @sides = [@side_1_length, @side_2_length, @side_3_length].sort
+
+  end 
+
+  class TriangleError < StandardError
+  end 
+
+  def kind 
+    if sides.uniq.length == 1 && sides.uniq[0] > 0
+      :equilateral
+    elsif sides.uniq.length == 2 && ((sides[0] + sides[1]) > sides[2])
+      :isosceles
+    elsif (sides[0] + sides[1]) > sides[2]
+      :scalene
+    else   
+      raise TriangleError
+    end 
+  end 
+
 end
